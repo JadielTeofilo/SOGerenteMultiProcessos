3062bd4bc462d9520dd375648d834cf5ccd25283
JadielTeofilo <teofilojadiel@gmail.com>
2017-04-23 20:32:32 -0300

    num deu

---
+++ escalonador.c
@@ -0,0 +1,65 @@
+struct jobTable
+{
+	int job;
+	char * arq_exec;
+	char * data;
+
+}typedef jobTableType;
+
+
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+void enviar_num_job(int job_anterior, int idfila){
+
+   	msgsnd(idfila, &job_anterior, sizeof(job_anterior)-sizeof(long), 0);
+}
+
+int criar_fila(){
+
+   	int idfila = 0; 
+	/* cria a fila*/
+   	if ((idfila = msgget(0x1223, IPC_CREAT|0x1B6)) < 0)
+   	{
+     	printf("erro na criacao da fila\n");
+     	exit(1);
+   	}
+   	// printf("id escalonar = %d\n", idfila);
+   	return idfila;
+}
+
+void excluir_fila(int idfila){
+   	msgctl(idfila, IPC_RMID, 0);
+}
+
+
+void escalonar(){
+	int job_anterior = -1;
+	int idfila = -1;
+
+   	idfila = criar_fila();
+   	//envia de inicio para a fila de jobs utilizados o -1
+   	enviar_num_job(job_anterior, idfila);
+
+
+   	sleep(60);
+
+
+   	excluir_fila(idfila);
+}
+
+
+
+
+int main(int argc, char const *argv[])
+{
+	escalonar();
+
+	return 0;
+}
--- executa_postergado.c
+++ executa_postergado.c
@@ -15,7 +15,23 @@ Colocar na estrutura de dados compartilhado
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <time.h>
 
+struct jobTable
+{
+	int job;
+	char * arq_exec;
+	time_t data;
+
+}typedef jobTableType;
+
+
+
+//Verifica se o numero de segundos eh um numero
 int is_num(const char * argv){
 	int tam = strlen(argv);
 
@@ -31,6 +47,12 @@ int is_num(const char * argv){
 
 int main(int argc, char *argv[])
 {
+	time_t current_time;
+	char* c_time_string;
+	int job_anterior = -1;
+	int idfila;
+	jobTableType mensagem;
+
 	//Verifica o numero de parametros
 	if (argc != 3){
 		printf("Num de parametros invalido\n");
@@ -43,11 +65,33 @@ int main(int argc, char *argv[])
 	//Verifica se o numero de segundos eh um numero
 	is_num(seg);
 
-	
 
+	if(msgget(0x1223, 0x1B6) < 0){
+		printf("Nenhuma fila encontrada \n");
+		exit(1);
+	}
 
+	idfila = msgget(0x1223, 0x1B6);
+	// printf("%d\n", idfila);
 
+	//Recebe o ultimo job
+	if (msgrcv(idfila, &job_anterior, sizeof(job_anterior)-sizeof(long), 0, IPC_NOWAIT) < 0){
+		printf("Nenhum numero de job encontrado na fila\n");
+		exit(1);
+	}
+   	// printf("mensagem recebida = %d\n", job_anterior);
 
+	//Criar estrutura para colocar arq_exec novo job e data
+	mensagem.job = job_anterior + 1;
+	mensagem.arq_exec = (char*) malloc(strlen(argv[2])*sizeof(char));
+    /* Obtain current time. */
+	mensagem.data = time(NULL) + atoi(argv[1]);
+	 
+    /* Conveter para string a data*/
+    c_time_string = ctime(&mensagem.data);
+    printf("job = %d\n", mensagem.job);
+    printf("arquivo = %s\n", mensagem.arq_exec);
+    printf("%s\n", c_time_string);
 
 	return 0;
 }
