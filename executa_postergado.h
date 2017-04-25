ad0d63ef0720dbe0614eec293fac68003c383b84
JadielTeofilo <teofilojadiel@gmail.com>
2017-04-25 16:35:30 -0300

    .h para o executa postergado

---
+++ executa_postergado.h
@@ -0,0 +1,42 @@
+/***
+Esse arquivo contem as definicoes de dados e inclusoes para 
+o programa executa postergado 
+
+
+Input 
+
+<seg>: delay de execucao em relacao a hora corrente
+<arq_exec>: Nome do arquivo a ser executado 
+
+Output
+
+Atribuir um numero de job unico a tupla <arq_exec> 
+Colocar na estrutura de dados compartilhado 
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <time.h>
+
+struct jobTable
+{
+	int job;
+	char * arq_exec;
+	time_t data;
+
+}typedef jobTableType;
+
+struct jobNum
+{
+	int job_num;
+
+}typedef jobNumType;
+
+
+
